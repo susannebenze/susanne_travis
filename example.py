e27ab6a9ab4188970d5d6445cb0840b053466ffd
Sszon <patrik.larses@gmail.com>
2017-02-21 21:38:56 +0100

    restore test_get_word_lengths; fixes #1

--- example.py
+++ example.py
@@ -18,8 +18,8 @@ def get_word_lengths(s):
     Returns a list of integers representing
     the word lengths in string s.
     """
-    # uncomment next line in step 9
-#   return [len(word) for word in s.split()]
+    # uncomment next line in step 9	
+    return [len(word) for word in s.split()]
     return None
 
 
